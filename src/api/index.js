88f87849ee784291e2053b905874b93841fa4d14
Ayudh Abhale <ayudhabhale65@gmail.com>
2024-10-18 17:21:30 +0530

    refactor(whole module): purged unnecessary files

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,4 @@
-import getIndiHospital from './hospital/getIndiHospital'
-import updateIndiHospital from './hospital/updateIndiHospital'
 import logoutAPi from './logout'
-import getIndiOperator from 'api/operator/getIndiOperator'
 // import getIndiStudents from './student/getIndiStudents'
 import getIndiAdmin from './superadmin/getIndiAdmin'
 import getAllStudents from './superadmin/getAllStudents'
@@ -9,9 +6,6 @@ import getAllProfessors from './superadmin/getAllProfessors'
 import getAllStaff from './superadmin/getAllStaffs'
 import getAllVendors from './superadmin/getAllVendors'
 export {
-  getIndiHospital,
-  updateIndiHospital,
-  getIndiOperator,
   logoutAPi,
   getIndiAdmin,
   getAllStudents,
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,12 @@
-import { useRoutes } from 'react-router-dom'
 import useAuthValidation from 'core/Private/Private'
+import { useRoutes } from 'react-router-dom'
 import SuperAdminRoutes from './SuperAdminRoutes'
-import HospitalRoutes from './HospitalRoutes'
 import commonRoutes from './commonRoutes'
-import OperatorRoutes from './OperatorRoutes'
 
 export default function Router() {
   useAuthValidation()
   const routes = useRoutes([
     ...SuperAdminRoutes,
-    ...HospitalRoutes,
-    ...OperatorRoutes,
     ...commonRoutes,
   ])
 
