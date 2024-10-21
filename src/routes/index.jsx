899da38b2952c7bb3deebf9da13715b021a442c0
Ayudh Abhale <ayudhabhale65@gmail.com>
2024-10-21 17:05:19 +0530

    feat(added prof and staff routes): added professor routes and staff routes to router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,8 @@ import useAuthValidation from 'core/Private/Private'
 import { useRoutes } from 'react-router-dom'
 import SuperAdminRoutes from './SuperAdminRoutes'
 import StudentsRoutes from './StudentRoutes'
+import ProfessorsRoutes from './ProfessorRoutes'
+import StaffRoutes from './StaffRoutes'
 import commonRoutes from './commonRoutes'
 
 export default function Router() {
@@ -10,6 +12,8 @@ export default function Router() {
     ...SuperAdminRoutes,
     ...StudentsRoutes,
     ...commonRoutes,
+    ...ProfessorsRoutes,
+    ...StaffRoutes,
   ])
 
   return routes
